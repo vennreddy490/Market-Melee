724d3b67af30b2eaeaa4d34b07342150fe5ce2bf
vennreddy490 <vennreddy490@gmail.com>
2024-09-28 00:07:50 -0400

    Cleaned up code, added comments

--- main.py
+++ main.py
@@ -8,39 +8,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+
+# Get secret key for sessions, Bcrypt for pswd hashing
 app.secret_key = os.environ.get('SECRET_KEY')
+bcrypt = Bcrypt(app)
 
-
-
+# Form MongoDB connection
 uri = os.environ.get('MONGO_URI')
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['sample_mflix']
-# test_collection = db['test_users']
 test_collection = db['test_users']
 
-bcrypt = Bcrypt(app)
 
+# Test MongoDB connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
 
-# user_data = {
-#     'user': 'test_user_6',
-#     'password': "abc123",
-#     'collection_of_stocks': [],
-#     'allocations': [],
-#     'portfolio_value': 10000        
-# }
 
-# test_collection.insert_one(user_data)
-
-
+# Base Route
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# Registers new user, adds them to MongoDB users 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -61,6 +54,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
+# Logs in users
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -76,14 +70,16 @@ def login():
             return redirect(url_for('login'))
     return render_template('login.html')
 
+# Dashboard Route (To be implemented later)
 @app.route("/dashboard")
 def dashboard():
     return "<p>Hello, World!</p>"
 
+# Tests Mongo_db connection, used for debugging
 @app.route("/mongo_test")
 def mongo_test():
     user_data = {
-        'user': 'test_user_1',
+        'user': 'test_user_xxx',
         'password': "abc123",
         'collection_of_stocks': [],
         'allocations': [],
