dae672fd40bfa2ee4d2dd802a5502ef8d935d9b6
Venn Reddy <vennreddy490@gmail.com>
2024-12-15 03:20:16 -0500

    renamed main.py to app.py to match convention, deleted app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ uri = os.environ.get('MONGO_URI')
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['sample_mflix']
 test_collection = db['test_users']
-
+stock_metrics_collection = db['stock_metrics']
 
 # Test MongoDB connection
 try:
@@ -128,27 +128,36 @@ def dashboard():
     sp500_dataFrame = pd.read_csv('sp500_symbols.csv')
     sp500_symbols = sp500_dataFrame['Symbol'].tolist()
     valid_stock = False
+    # Information for Stock Stats
+    stock_image = None
+    stock_stats = None
+
     # Handle form submission to generate stock stats
     stock_stats = None 
     if request.method == 'POST':
-        stock_selected = request.form.get('symbolInput')
-        if stock_selected:
-            # Calculate Stock Statistics
-            stock_stats = calculate_stock_stats(stock_selected)
+        stock_selected = request.form['symbol'].upper()
+        print(stock_selected)
+        if stock_selected in sp500_symbols:
+            valid_stock = True
 
-            # PlaceHolder
-            stock_stats = {
-                'Daily Returns': 'Placeholder',
-                'Cumulative Returns': 'Placeholder',
-                'Volatility': 'Placeholder',
-                'Sharpe Ratio': 'Placeholder',
-                'Sortino Ratio': 'Placeholder',
-                'Tracking Error': 'Placeholder',
-            }
-            if stock_selected in sp500_symbols:
-                valid_stock = True
+            # Retrieve Stock Data from MongoDB
+            stock_data = stock_metrics_collection.find_one({'stock_name': stock_selected})
 
-    return render_template('dashboard.html', user=user, sp500_symbols=sp500_symbols, stock_stats=stock_stats, valid_stock=valid_stock)
+            if stock_data:
+                stock_data.pop('id', None)
+                stock_stats = stock_data
+
+                # Get Graph Plot
+                plot_solo_stock(stock_selected)
+                image_path = f"{stock_selected}_returns.png"
+                stock_image = f'stock_graphs/{image_path}'
+            else:
+                flash(f"Data for {stock_selected} not found in database.")
+        else:
+            flash(f"{stock_selected} is not in the S&P 500")
+                
+
+    return render_template('dashboard.html', user=user, sp500_symbols=sp500_symbols, stock_stats=stock_stats, valid_stock=valid_stock, stock_image=stock_image)
 
 # Tests Mongo_db connection, used for debugging
 @app.route("/mongo_test")
@@ -171,12 +180,21 @@ def mongo_test():
 
     return "<p>This should have added a user.</p>"
 
-@app.route("/user_data_test")
-def user_data_test():
+@app.route("/league")
+def league():
     username = session.get('username')
     user_data = test_collection.find_one({'user': username})
 
     print(f"The user is: {username}")
+
+    # print("Before: Calling find_recent_leaders")
+    # top_three = find_historical_leaders(username)
+
+    # print(f"The type of top_three is: {type(top_three)}")
+    # print(f"top_three is: {top_three}")
+
+    # plot_user_vs_top_three_historical(username, top_three)
+    # print("After: Calling find_recent_leaders")
 
     allocs = user_data.get('allocations', [])
     sv = user_data.get('portfolio_value', 0)
@@ -195,10 +213,6 @@ def user_data_test():
     allocs = [alloc / 100 for alloc in allocs]
     sv = user_data.get('portfolio_value', 0)
 
-    print(f"\nHere is the value getting passed in for allocs: {allocs}")
-    print(f"\nHere is the value getting passed in for sv: {sv}")
-
-
     port_val = get_portfolio_returns(df_prices, allocs, sv)
     print(f"\nget_portfolio_returns() returns: \n{port_val.head(10)}")
 
@@ -216,23 +230,71 @@ def user_data_test():
     port_val_df.columns = ['Date', 'Portfolio']
     port_val_df.to_csv(file_path, index=False)
 
-    # Plot the user portfolio and save the image
-    plot_user_portfolio(username)
 
-    # Serve the image and display it to the frontend
-    image_filename = f"{username}_portfolio_graph.png"
+    # RUNNING COMPARE TO WEEKLY LEADERS AND PLOT THEM
+    print("Before: Calling find_recent_leaders")
+    top_three_weekly = find_weekly_leaders(username)
+
+    print(f"The type of top_three is: {type(top_three_weekly)}")
+    print(f"top_three is: {top_three_weekly}")
+
+    plot_user_vs_top_three_weekly(username, top_three_weekly)
+    print("After: Calling find_recent_leaders")
+
+
+    # Store the weekly image and display it to the frontend
+    image_filename_weekly = f"temp_graphs_weekly/{username}_vs_leaders.png"
 
     # Check if the image exists before serving
-    image_path = os.path.join(GRAPH_DIR, image_filename)
-    if os.path.exists(image_path):
-        return render_template('display_image.html', username=username)
+    image_static_path = 'static/' + image_filename_weekly
+    if os.path.exists(image_static_path):
+        print("Valid weekly image")
     else:
         return f"Image for {username} not found.", 404
 
-    return "<p>This should have created a dataframe and printed it in the console.</p>"
+
+    # RUNNING COMPARE TO HISTORICAL LEADERS AND PLOT THEM
+    print("Before: Calling find_historical_leaders")
+    top_three_historical = find_historical_leaders(username)
+
+    print(f"The type of top_three is: {type(top_three_historical)}")
+    print(f"top_three is: {top_three_historical}")
+
+    plot_user_vs_top_three_historical(username, top_three_historical)
+    print("After: Calling find_historical_leaders")
+
+
+    # Store the historical image and display it to the frontend
+    image_filename_historical = f"temp_graphs/{username}_vs_leaders.png"
+
+    # Check if the image exists before serving
+    image_static_path = 'static/' + image_filename_historical
+    if os.path.exists(image_static_path):
+        print("Valid historical image")
+    else:
+        return f"Image for {username} not found.", 404
+
+
+    # PLOT THE USER PORTFOLIO AND SAVE THE IMAGE
+    # TODO: SEND TO TEMPLATE AND PLOT SPY IN THAT METHOD 
+    plot_user_portfolio(username)
+
+    # Store the personal image and display it to the frontend
+    image_filename_personal = f"user_portfolio_graphs/{username}_portfolio_graph.png"
+
+    # Check if the image exists before serving
+    image_static_path = 'static/' + image_filename_personal
+    if os.path.exists(image_static_path):
+        return render_template('league.html', username=username, weekly_image=image_filename_weekly,
+                               historical_image=image_filename_historical, personal_image=image_filename_personal,
+                               top_weekly=top_three_weekly, top_historical=top_three_historical)
+    else:
+        return f"Image for {username} not found.", 404
+
     
 @app.route('/portfolio_image')
 def serve_image():
+    print("IF YOU SEE THIS DEBUG STATEMENT, /portfolio_image is being hit, when it should not be.")
     username = session.get('username')
     # Construct the image filename
     image_filename = f"{username}_portfolio_graph.png"
@@ -240,3 +302,10 @@ def serve_image():
     # Serve the image from the user portfolio directory
     return send_from_directory(GRAPH_DIR, image_filename)
 
+@app.route('/logout')
+def logout():
+    # Clear the user session
+    session.pop('username', None)
+    # session.clear()
+    # Redirect to the welcome page
+    return redirect(url_for('welcome'))
